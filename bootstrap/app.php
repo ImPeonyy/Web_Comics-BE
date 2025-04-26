7b20ac00679b022c505375344dd6928dc11b5c1d
Peonyy~ <thieuthang1412@gmail.com>
2025-04-26 21:34:46 +0700

    Fix: fix Check Self, Admin middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'self' => \App\Http\Middleware\CheckSelf::class,
+            'admin' => \App\Http\Middleware\CheckAdmin::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
